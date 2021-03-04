48f86f72afca3c6c58967ac4d3cb7e3ce5c98b3b
Pedro Rodrigues <medecau@gmail.com>
2021-03-04 08:13:43 +0100

    Add a way of hiding the redirect based on user-agent

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from bottle import Bottle, redirect, static_file, template
+import re
+from bottle import Bottle, redirect, request, static_file, template
 
 
 app = Bottle()
+
+with open("blocklist.txt") as fp:
+    blocklist = {line.strip() for line in fp}
+
+all_or_patterns = "|".join(blocklist)
+block = re.compile(f".*(?:{all_or_patterns}).*")
 
 
 @app.route("/robots.txt")
@@ -16,4 +23,14 @@ def index():
 
 @app.route("/*/<url:re:.+>")
 def index(url):
+    user_agent = request.headers.get("User-Agent")
+
+    # the match will return None if we do not recognise the user-agent
+    # thus if the match does return something other than None
+    # we want to hide the redirect
+    hide_redirect = block.match(user_agent.lower()) is not None
+
+    if hide_redirect:
+        return f"🖕"
+
     redirect(url)

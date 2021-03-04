4107a60bf363470ad4ffef54fc935de8fa1fa488
Pedro Rodrigues <medecau@gmail.com>
2021-03-04 08:29:49 +0100

    Add a check for allowed domains - avoid abuse + redir to issues page

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import re
+from urllib.parse import urlsplit
 from bottle import Bottle, redirect, request, static_file, template
 
 
-app = Bottle()
+ISSUE_PAGE = "https://github.com/medecau/sevenproxies/issues"
 
-with open("blocklist.txt") as fp:
-    blocklist = {line.strip() for line in fp}
 
-all_or_patterns = "|".join(blocklist)
+def load_items(path):
+    with open(path) as fp:
+        return {line.strip() for line in fp}
+
+
+all_or_patterns = "|".join(load_items("blocklist.txt"))
 block = re.compile(f".*(?:{all_or_patterns}).*")
+del all_or_patterns
+
+allow = load_items("allowlist.txt")
+
+
+app = Bottle()
 
 
 @app.route("/robots.txt")
@@ -23,6 +33,12 @@ def index():
 
 @app.route("/*/<url:re:.+>")
 def index(url):
+    url_parts = urlsplit(url)
+    allow_redirect = url_parts.hostname in allow
+
+    if not allow_redirect:
+        redirect(ISSUE_PAGE)
+
     user_agent = request.headers.get("User-Agent")
 
     # the match will return None if we do not recognise the user-agent
